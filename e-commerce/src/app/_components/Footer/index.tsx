51b0beaf8dd5c5596240a89637fe837e82e1347b
SPEEDYEEEEE <speedyeeeeeee69@gmail.com>
2023-12-22 15:25:15 +0500

    Initial commit
    Develop an exceptional e-commerce website complete with an intuitive admin dashboard and a robust Content Management System (CMS) using the latest Next.js version (Next 14). The platform will seamlessly integrate with Stripe for secure payment processing and leverage the power of TypeScript for enhanced development efficiency. The CMS functionality will be powered by Payload CMS, ensuring dynamic content management and easy customization. This comprehensive solution aims to provide a seamless and feature-rich online shopping experience, combining cutting-edge technologies to deliver a top-tier e-commerce webshop.

---
+++ e-commerce/src/app/_components/Footer/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'next/link'
+import { Footer } from '../../../payload/payload-types'
+import { fetchFooter } from '../../_api/fetchGlobals'
+import FooterComponent from './FooterComponent'
+// import { ThemeSelector } from '../../_providers/Theme/ThemeSelector'
+// import { Gutter } from '../Gutter'
+// import { CMSLink } from '../Link'
+
+export async function Footer() {
+  let footer: Footer | null = null
+
+  try {
+    footer = await fetchFooter()
+  } catch (error) {
+    console.error(error);
+  }
+
+  const navItems = footer?.navItems || []
+
+  return (
+    <>
+      <FooterComponent footer={footer}/>
+    </>
+  )
+}
